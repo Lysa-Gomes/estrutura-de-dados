531a645c2a770cb484aa8bd0be846e01a12f3900
Lysa-Gomes <laysagomesmello9@gmail.com>
2022-06-14 21:57:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Laysa";
+console.log(typeof nome);
+let idade = 18;
+console.log(typeof idade);
+let ano = 2022;
+console.log(ano-idade)
+
+console.log("Eu estou mexendo no console.log")
+
+let quartaFeira = true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
